faf0fe7eb9594d94c1334e844ef8675e7d5a797c
Fedor Sharaputo <sharaputo@icloud.com>
2023-04-27 10:43:27 +0300

    add instagram portfolio card

---
+++ src/components/PortfolioInstagramCard.vue
@@ -0,0 +1,103 @@
+<template>
+  <article class="portfolio-card">
+    <div class="portfolio-card__image" @click.prevent="showLightbox">
+      <img :src="image" :alt="title" />
+    </div>
+
+    <button
+      type="button"
+      class="portfolio-card__title letter-spacing"
+      @click.prevent="showLightbox">
+      <span>
+        {{ title }}
+      </span>
+
+      <ArrowIcon />
+    </button>
+  </article>
+
+  <vue-easy-lightbox
+    :visible="isLightboxVisible"
+    :imgs="lightboxImages"
+    :loop="true"
+    :moveDisabled="true"
+    :rotateDisabled="true"
+    :swipeTolerance="10"
+    :minZoom="1"
+    @hide="onLightboxClose">
+    <template v-slot:prev-btn="{ prev }">
+      <button
+        type="button"
+        class="lightbox-arrow lightbox-arrow--prev"
+        @click="prev">
+        <LightboxArrow style="transform: rotate(180deg)" />
+      </button>
+    </template>
+
+    <template v-slot:next-btn="{ next }">
+      <button
+        type="button"
+        class="lightbox-arrow lightbox-arrow--next"
+        @click="next">
+        <LightboxArrow />
+      </button>
+    </template>
+  </vue-easy-lightbox>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  import ArrowIcon from '@/components/icons/ArrowIcon.vue';
+  import LightboxArrow from '@/components/icons/LightboxArrow.vue';
+
+  interface Props {
+    image: string;
+    title: string;
+    lightboxImages: string[];
+  }
+
+  defineProps<Props>();
+
+  // Lighbox related logic
+  const isLightboxVisible = ref(false);
+
+  const showLightbox = () => {
+    isLightboxVisible.value = true;
+  };
+  const onLightboxClose = () => (isLightboxVisible.value = false);
+</script>
+
+<style lang="less" scoped>
+  .portfolio-card {
+    display: grid;
+    gap: 10px;
+
+    &__image {
+      cursor: pointer;
+    }
+    &__title {
+      display: grid;
+      grid-template-columns: 1fr 10px;
+      align-items: center;
+      gap: 20px;
+      font-size: 14px;
+      line-height: 95%;
+      text-align: left;
+      color: @text-primary;
+      padding-bottom: 7px;
+      border-bottom: 1px solid @black;
+    }
+  }
+
+  @media @xlarge-min {
+    .portfolio-card {
+      gap: 20px;
+
+      &__title {
+        font-size: 16px;
+        padding-bottom: 15px;
+      }
+    }
+  }
+</style>

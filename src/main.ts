bd8d19925eb0921864e232b00ab074b43f999d96
Fedor Sharaputo <sharaputo@icloud.com>
2023-04-27 16:54:43 +0300

    remove animation plugins

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,10 @@ import '@/assets/css/style.css';
 // Components
 import App from '@/App.vue';
 import AppContainer from '@/components/base/AppContainer.vue';
-import SectionTitle from '@/components/base/SectionTitle.vue';
 
 // Plugins
 import VueEasyLightbox from 'vue-easy-lightbox';
-import { MotionPlugin } from '@vueuse/motion';
+import VueAnimateOnScroll from 'vue-animate-onscroll';
 
 const app = createApp(App);
 // Use core
@@ -21,10 +20,9 @@ app.use(router);
 
 // Use components
 app.component('AppContainer', AppContainer);
-app.component('SectionTitle', SectionTitle);
 
 // Use plugins
 app.use(VueEasyLightbox);
-app.use(MotionPlugin);
+app.use(VueAnimateOnScroll);
 
 router.isReady().then(() => app.mount('#app'));

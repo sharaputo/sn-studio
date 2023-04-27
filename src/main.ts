0a11e6c360856690f0970b2502731145b18c29f6
Fedor Sharaputo <sharaputo@icloud.com>
2023-04-27 10:45:29 +0300

    add vue motion plugin

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+
+// Core
 import { router } from '@/router';
 
 // Styles
@@ -11,13 +13,18 @@ import SectionTitle from '@/components/base/SectionTitle.vue';
 
 // Plugins
 import VueEasyLightbox from 'vue-easy-lightbox';
+import { MotionPlugin } from '@vueuse/motion';
 
 const app = createApp(App);
+// Use core
 app.use(router);
 
+// Use components
 app.component('AppContainer', AppContainer);
 app.component('SectionTitle', SectionTitle);
 
+// Use plugins
 app.use(VueEasyLightbox);
+app.use(MotionPlugin);
 
 router.isReady().then(() => app.mount('#app'));

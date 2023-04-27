81a2f21e13ddf7192ab1da8d7f6c0ad72f5247eb
Fedor Sharaputo <sharaputo@icloud.com>
2023-04-27 08:05:38 +0300

    add vue-lightbox

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,14 @@ import AppContainer from '@/components/base/AppContainer.vue';
 import SectionTitle from '@/components/base/SectionTitle.vue';
 
 // Plugins
+import VueEasyLightbox from 'vue-easy-lightbox';
 
 const app = createApp(App);
 app.use(router);
 
 app.component('AppContainer', AppContainer);
 app.component('SectionTitle', SectionTitle);
+
+app.use(VueEasyLightbox);
 
 router.isReady().then(() => app.mount('#app'));

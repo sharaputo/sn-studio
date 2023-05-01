4e1d3f733d8620ac05e2134c715896c1f8e6fc57
Fedor Sharaputo <sharaputo@icloud.com>
2023-05-01 11:47:27 +0300

    add swipe down to close

--- src/components/base/ServiceHint.vue
+++ src/components/base/ServiceHint.vue
@@ -10,7 +10,7 @@
     </button>
 
     <Transition name="slide-up">
-      <ul class="service-hint__list" v-if="isHintShown" @click="toggleHint">
+      <ul class="service-hint__list" v-if="isHintShown">
         <li class="service-hint__item" v-for="(item, i) in list" :key="i">
           {{ item }}
         </li>
@@ -20,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-  import { ref, computed } from 'vue';
+  import { ref, computed, onMounted, onUnmounted } from 'vue';
 
   interface Props {
     list: string[];
@@ -35,8 +35,10 @@
   const desktopRes = window.matchMedia('(min-width: 1200px)');
   const mousePointer = window.matchMedia('(pointer: fine)');
   const touchPointer = window.matchMedia('(pointer: coarse)');
+  const scrolledFromTop = ref(0);
   const onHintClick = () => {
     if (!desktopRes.matches && touchPointer.matches) {
+      scrolledFromTop.value = window.scrollY;
       toggleHint();
     }
   };
@@ -45,10 +47,30 @@
       toggleHint();
     }
   };
+  const closeHint = () => {
+    if (!desktopRes.matches && touchPointer.matches) {
+      const positionFromTop = window.scrollY;
+
+      if (positionFromTop - scrolledFromTop.value > 50)
+        isHintShown.value = false;
+      if (positionFromTop < scrolledFromTop.value) {
+        isHintShown.value = false;
+      }
+
+      isHintShown.value = false;
+    }
+  };
 
   const isActive = computed(() => ({
     'service-hint--active': isHintShown.value,
   }));
+
+  onMounted(() => {
+    window.addEventListener('scroll', closeHint);
+  });
+  onUnmounted(() => {
+    window.removeEventListener('scroll', closeHint);
+  });
 </script>
 
 <style lang="less" scoped>
@@ -88,9 +110,27 @@
         position: fixed;
         right: 0;
         bottom: 0;
+        padding-top: 50px;
+        padding-bottom: 30px;
         border-top-right-radius: 10px;
         border-top-left-radius: 10px;
+
+        &::before {
+          content: '';
+          position: absolute;
+          top: 20px;
+          left: calc(50% - 50px);
+          width: 100px;
+          height: 3px;
+          background-color: @dove-gray;
+          opacity: 0.5;
+          border-radius: 10px;
+          z-index: 10;
+        }
       }
+    }
+    .service-hint--active .service-hint__list::before {
+      animation: pop-down @anim-long 0.5s;
     }
 
     .slide-up-enter-active {
@@ -140,6 +180,25 @@
         opacity: 1;
         transform: scale(1);
       }
+    }
+  }
+</style>
+<style>
+  @keyframes pop-down {
+    0% {
+      transform: translateY(0);
+    }
+    25% {
+      transform: translateY(2px);
+    }
+    50% {
+      transform: translateY(0);
+    }
+    75% {
+      transform: translateY(2px);
+    }
+    100% {
+      transform: translateY(0);
     }
   }
 </style>
